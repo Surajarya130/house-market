95be00670ef2038ff6d50827b47c6ddaf94c207c
surajarya130 <surajarya130@gmail.com>
2022-12-16 18:38:21 +0530

    #1 16th Dec

--- src/pages/TestAxios.jsx
+++ src/pages/TestAxios.jsx
@@ -3,22 +3,53 @@ import "./TestAxios.css";
 import axios from "axios";
 
 function TestAxios() {
-  const [data, setData] = useState(null);
+  const [text, setText] = useState("");
+  // const handleClick = () => {
+  // let config = {
+  //   url: "https://jsonplaceholder.typicode.com/posts",
+  //   method: "GET",
+  //   headers: {},
+  // };
+
+  // axios(config)
+  //   .then((jsData) => {
+  //     // console.log(jsData);
+  //     setData(jsData.data);
+  //   })
+  //   .catch((err) => console.log("Error Occured"));
+
+  // let config = (country) => {
+  //   return {
+  //     url: `http://universities.hipolabs.com/search?country=${country}`,
+  //     headers: {},
+  //     method: "GET",
+  //   };
+  // };
+
+  // let getData = async () => {
+  //   let res = await axios(config("india"));
+  //   let data = res.data;
+  //   // console.log(data);
+  //   setData(data);
+  // };
+  // getData();
+  // };
+  // console.log(data);
+
   const handleClick = () => {
     let config = {
       url: "https://jsonplaceholder.typicode.com/posts",
-      headers: {},
-      method: "GET",
+      method: "POST",
+      header: {},
+      data: JSON.stringify({
+        name: "Suraj",
+        age: 25,
+        add: "Town, Rbj, SBDR",
+      }),
     };
-
-    axios(config)
-      .then((jsData) => {
-        setData(jsData.data);
-      })
-      .catch((err) => console.log("Error Occured"));
+    axios(config).then((res) => console.log(res));
   };
 
-  console.log(data);
   return (
     <>
       <div className="testContainer">
@@ -26,13 +57,23 @@ function TestAxios() {
         <button type="button" onClick={handleClick}>
           Fetch Path
         </button>
-        {data &&
+
+        <input
+          style={{ display: "none" }}
+          type="text"
+          placeholder="Element..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+
+        {/* {data &&
           data.map((item) => (
-            <p key={item.id}>
-              <span>No: {item.id}</span>
-              {item.userId} - <b>{item.title}</b>
+            // <p key={item.id}>
+            <p>
+              <span>No: {Math.floor(Math.random() * 10)}</span>
+              {item.alpha_two_code} - <b>{item.name}</b>
             </p>
-          ))}
+          ))} */}
       </div>
     </>
   );
